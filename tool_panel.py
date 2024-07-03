a25825dcd71ac1367bc574cffe55908f145c2369
Mohammad <roze.sorkh.777@gmail.com>
2024-07-04 02:44:18 +0330

    0.7.3

--- tool_panel.py
+++ tool_panel.py
@@ -75,38 +75,43 @@ class ColorSlides(ctk.CTkFrame):
 
         self.grid(row=0, column=0, padx=(5, 30))
 
-        self.red_slider = ctk.CTkSlider(self,
-                                        button_color=SLIDER_RED,
-                                        hover=False,
-                                        from_=0,
-                                        to=15,
-                                        variable=self.red_var,
-                                        command=self.update_color)
-        self.red_slider.pack(pady=5)
+        self.create_widgets()
 
-        self.green_slider = ctk.CTkSlider(self,
-                                          button_color=SLIDER_GREEN,
-                                          hover=False,
-                                          from_=0,
-                                          to=15,
-                                          variable=self.green_var,
-                                          command=self.update_color)
-        self.green_slider.pack(pady=5)
+    def create_widgets(self):
+        red_slider = ctk.CTkSlider(self,
+                                   button_color=SLIDER_RED,
+                                   hover=False,
+                                   from_=0,
+                                   to=15,
+                                   variable=self.red_var,
+                                   command=self.update_color)
+        red_slider.pack(pady=5)
 
-        self.blue_slider = ctk.CTkSlider(self,
-                                         button_color=SLIDER_BLUE,
-                                         hover=False,
-                                         from_=0,
-                                         to=15,
-                                         variable=self.blue_var,
-                                         command=self.update_color)
-        self.blue_slider.pack(pady=5)
+        green_slider = ctk.CTkSlider(self,
+                                     button_color=SLIDER_GREEN,
+                                     hover=False,
+                                     from_=0,
+                                     to=15,
+                                     variable=self.green_var,
+                                     command=self.update_color)
+        green_slider.pack(pady=5)
+
+        blue_slider = ctk.CTkSlider(self,
+                                    button_color=SLIDER_BLUE,
+                                    hover=False,
+                                    from_=0,
+                                    to=15,
+                                    variable=self.blue_var,
+                                    command=self.update_color)
+        blue_slider.pack(pady=5)
 
     def update_color(self, *args):
+        # Generate an event that will activate brush
         self.event_generate('<<activate_brush>>')
-        red = COLOR_RANGE[int(self.red_slider.get())]
-        green = COLOR_RANGE[int(self.green_slider.get())]
-        blue = COLOR_RANGE[int(self.blue_slider.get())]
+
+        red = COLOR_RANGE[int(self.red_var.get())]
+        green = COLOR_RANGE[int(self.green_var.get())]
+        blue = COLOR_RANGE[int(self.blue_var.get())]
 
         color = f'#{red}{green}{blue}'
         self.color_string.set(color)
@@ -131,16 +136,16 @@ class BrushPreview(ctk.CTkCanvas):
 
         self.draw_circle()
 
-    def draw_circle(self, *args):
+    def draw_circle(self, *args) -> None:
         if self.brush_is_on.get():
-            color = self.color_string.get()
-            width = 0
+            color: str = self.color_string.get()
+            width: int = 0
         else:
-            color = BRUSH_PREVIEW_BG
-            width = 1
+            color: str = BRUSH_PREVIEW_BG
+            width: int = 1
 
         self.delete('all')
-        r = int(self.brush_size_var.get() * 0.7 // 2)
+        r: int = int(self.brush_size_var.get() * 0.7 // 2)
         # x and y for center of oval
         x, y = 35, 35
         self.create_oval(x - r, y - r, x + r, y + r, width=width, fill=color)
@@ -161,29 +166,42 @@ class ColorButtons(ctk.CTkFrame):
         self.create_buttons()
 
     def create_layout(self):
-        self.columnconfigure(tuple(range(4)), weight=1, uniform='a')
-        self.rowconfigure(tuple(range(6)), weight=1, uniform='b')
+        self.columnconfigure(tuple(range(COLOR_COLS)), weight=1, uniform='a')
+        self.rowconfigure(tuple(range(COLOR_ROWS)), weight=1, uniform='b')
 
     def create_buttons(self):
         for row in range(COLOR_ROWS):
             for col in range(COLOR_COLS):
-                button_color = f'#{COLORS[row][col]}'
+                button_color: str = f'#{COLORS[row][col]}'
                 ctk.CTkButton(self,
                               text='',
                               fg_color=button_color,
                               hover=False,
                               corner_radius=0,
-                              command=lambda color=button_color: self.change_color(color)).grid(row=row,
-                                                                                                column=col,
-                                                                                                sticky='news')
+                              command=lambda color=button_color: self.change_color(color)
+                              ).grid(row=row,
+                                     column=col,
+                                     sticky='news',
+                                     padx=0.4,
+                                     pady=0.4)
 
-    def change_color(self, color):
+    def change_color(self, color: str) -> None:
+        """
+        Changes the brush color to the selected color.
+        It also updates the position of color sliders.
+
+        :param color: a string that represents the color as hexadecimal
+        :return: None
+        """
+        # Activates the brush if not selected
         self.event_generate('<<activate_brush>>')
         self.color_string.set(color)
 
-        red = COLOR_RANGE.index(color[1])
-        green = COLOR_RANGE.index(color[2])
-        blue = COLOR_RANGE.index(color[3])
+        # Gets the decimal equivalent of each hexadecimal number
+        # And uses it to update the sliders positions.
+        red: int = int(color[1], 16)
+        green: int = int(color[2], 16)
+        blue: int = int(color[3], 16)
 
         self.red_var.set(red)
         self.green_var.set(green)
@@ -208,41 +226,23 @@ class ActionButtons(ctk.CTkFrame):
         self.parent = parent
         self.is_brush_active = is_brush_active
         self.color_string = color_string
-        self.last_color = None
+        self.last_color: None | str = None
         self.parent.bind('<<activate_brush>>', lambda e: self.activate_brush())
 
         self.grid(row=3, column=0, columnspan=2, sticky='news', padx=5, pady=5)
 
-        # Images
-        brush_img = ctk.CTkImage(Image.open('images/brush.png'))
-        clear_img = ctk.CTkImage(Image.open('images/clear.png'))
-        eraser_img = ctk.CTkImage(Image.open('images/eraser.png'))
-
-        self.brush_btn = ctk.CTkButton(self,
-                                       text='',
-                                       image=brush_img,
-                                       fg_color=BUTTON_ACTIVE_COLOR,
-                                       hover_color=BUTTON_HOVER_COLOR,
-                                       width=55,
-                                       command=self.activate_brush)
+        self.brush_btn = Button(self, image='images/brush.png',
+                                command=self.activate_brush)
         self.brush_btn.pack(side='left', padx=(0, 5))
 
-        self.eraser_button = ctk.CTkButton(self,
-                                           text='',
-                                           image=eraser_img,
-                                           fg_color=BUTTON_COLOR,
-                                           hover_color=BUTTON_HOVER_COLOR,
-                                           width=55,
-                                           command=self.activate_eraser)
+        self.eraser_button = Button(self, image='images/eraser.png',
+                                    command=self.activate_eraser)
         self.eraser_button.pack(side='left', padx=5)
 
-        ctk.CTkButton(self,
-                      text='',
-                      image=clear_img,
-                      fg_color=BUTTON_COLOR,
-                      hover_color=BUTTON_HOVER_COLOR,
-                      width=55,
-                      command=self.clear_canvas).pack(side='right', padx=(5, 0))
+        Button(self, image='images/brush.png', command=self.clear_canvas).pack(side='right', padx=(5, 0))
+
+        # Activate brush on first run
+        self.activate_brush()
 
     def activate_brush(self):
         self.is_brush_active.set(True)
@@ -250,16 +250,12 @@ class ActionButtons(ctk.CTkFrame):
         if self.last_color:
             self.color_string.set(self.last_color)
         # Change the brush button color to active
-        self.brush_btn.configure(fg_color=BUTTON_ACTIVE_COLOR)
-        # Change the eraser button color to not active
-        self.eraser_button.configure(fg_color=BUTTON_COLOR)
+        self.mark_as_active()
 
     def activate_eraser(self):
         self.is_brush_active.set(False)
         # Change the eraser button color to active color
-        self.eraser_button.configure(fg_color=BUTTON_ACTIVE_COLOR)
-        # Change the brush button color to not active color
-        self.brush_btn.configure(fg_color=BUTTON_COLOR)
+        self.mark_as_active()
         # Save the color
         self.last_color = self.color_string.get()
         # Change the brush color to the background color
@@ -270,3 +266,34 @@ class ActionButtons(ctk.CTkFrame):
         self.event_generate('<<ClearCanvas>>')
         # Set the brush as active
         self.activate_brush()
+
+    def mark_as_active(self) -> None:
+        """
+        Changes the background of the active button
+        to indicate that it's active.
+
+        :return: None
+        """
+        # If brush is active
+        if self.is_brush_active.get():
+            active_btn = self.brush_btn
+            deactive_btn = self.eraser_button
+        # If eraser is active
+        else:
+            active_btn = self.eraser_button
+            deactive_btn = self.brush_btn
+
+        active_btn.configure(fg_color=BUTTON_ACTIVE_COLOR)
+        deactive_btn.configure(fg_color=BUTTON_COLOR)
+
+
+class Button(ctk.CTkButton):
+    def __init__(self, parent, image, **kwargs):
+        button_img = ctk.CTkImage(Image.open(image))
+        super().__init__(master=parent,
+                         text='',
+                         image=button_img,
+                         fg_color=BUTTON_COLOR,
+                         hover_color=BUTTON_HOVER_COLOR,
+                         width=55,
+                         **kwargs)
